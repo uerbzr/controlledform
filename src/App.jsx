3b135339c82df10153cc6dfdd5f0f98466039db1
uerbzr <nigel.sibbert@gmail.com>
2025-02-06 08:31:33 +0000

    updated form

--- src/App.jsx
+++ src/App.jsx
@@ -39,53 +39,106 @@ function App() {
 
   return (
     <>
-      <div>
-        <h1 className="display-2">Controlled Form</h1>
-        <hr />
-        <form onSubmit={handleSubmit}>
-          <h2>Complaining Form</h2>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h1 className="display-2">Controlled Form</h1>
+            <hr />
+            <form onSubmit={handleSubmit}>
+              <h2>Complaining Form</h2>
 
-          <label>
-            Full Name
-            <input
-              type="text"
-              name="name"
-              onChange={handleChange}
-              value={formData.name}
-              required
-              className="form-control"
-            />
-          </label>
-          <br />
-          <input type="submit" value="Submit!" className="btn btn-success" />
-        </form>
-        <hr />
+              <label className="form-control">
+                Full Name
+                <input
+                  type="text"
+                  name="name"
+                  onChange={handleChange}
+                  value={formData.name}
+                  required
+                  className="form-control"
+                />
+              </label>
 
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Address</th>
-              <th>Phone</th>
-              <th>Email</th>
-              <th>Complaint</th>
-              <th>Contact</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {complaints.map((complaint) => (
-              <tr key={complaint.id}>
-                <td>{complaint.name}</td>
-                <td>{complaint.address}</td>
-                <td>{complaint.phone}</td>
-                <td>{complaint.email}</td>
-                <td>{complaint.complaint}</td>
-                <td>{complaint.contact}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+              <label className="form-control">
+                Address
+                <input
+                  type="text"
+                  name="address"
+                  onChange={handleChange}
+                  value={formData.address}
+                  required
+                  className="form-control"
+                />
+              </label>
+
+              <label className="form-control">
+                Phone
+                <input
+                  type="tel"
+                  name="phone"
+                  onChange={handleChange}
+                  value={formData.phone}
+                  required
+                  className="form-control"
+                />
+              </label>
+
+              <label className="form-control">
+                Email
+                <input
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                  value={formData.email}
+                  required
+                  className="form-control"
+                />
+              </label>
+              <label htmlFor="complaint" className="form-control">
+                <textarea
+                  name="complaint"
+                  onChange={handleChange}
+                  value={formData.complaint}
+                  rows="10"
+                  placeholder="write your complaint"
+                ></textarea>
+              </label>
+              <br />
+              <input
+                type="submit"
+                value="Submit!"
+                className="btn btn-success"
+              />
+            </form>
+            <hr />
+
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Address</th>
+                  <th>Phone</th>
+                  <th>Email</th>
+                  <th>Complaint</th>
+                  <th>Contact</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {complaints.map((complaint) => (
+                  <tr key={complaint.id}>
+                    <td>{complaint.name}</td>
+                    <td>{complaint.address}</td>
+                    <td>{complaint.phone}</td>
+                    <td>{complaint.email}</td>
+                    <td>{complaint.complaint}</td>
+                    <td>{complaint.contact}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </>
   );
